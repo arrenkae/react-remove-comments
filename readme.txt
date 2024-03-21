499d3abd213445be86f3a24d736a673fce1e6180
slayerstokely <stokely@slayers.ru>
2024-03-21 16:49:24 +0200

    improved handling of line breaks in output

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Code complexity means a level of diffictulty in understanding, maintaining and modifying a particular piece of software code. It is influenced by various factors, such as the number of lines of code, number of methods and functions, nesting, inheritance etc. Reducing complexity is important to make the code readable and easier to maintain, simplify testing process and optimize performance. Measuring code complexity can be subjective, but there are different metrics and tools that help properly evaluate it.
+
+Complexity of my solution is represented by following factors:
+
+- 66 lines of code
+- 2 functional components within the App component
+- ReadFile component: 1 import, 5 functions/methods, 3 conditions, max nesting depth 1
+- Output component: 2 imports, 17 functions/methods, 1 loop, 7 conditions, max nesting depth 3
--- src/components/Output.js
+++ src/components/Output.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
 
 function Output() {
-    const [text, setText] = useState('No file uploaded');
+    const [text, setText] = useState("No file uploaded");
 
     useEffect(() => {
-        const savedText = localStorage.getItem('text');
+        const savedText = localStorage.getItem('text'); // text from the last uploaded txt file
         let i = 0;
         let newText = '';
         while (i < savedText.length) {
             if (savedText.charAt(i) == '/' && savedText.charAt(i+1) == '/') {
-                i += savedText.slice(i).indexOf('\n');
-                // searches the end of the comment and 
+                i += savedText.slice(i).indexOf('\n') + 1;
+                // skips indexes between opening and closing tags
             } else if (savedText.charAt(i) == '/' && savedText.charAt(i+1) == '*') {
                 if (savedText.slice(i).includes('*/')) {
                     i += savedText.slice(i).indexOf('*/') + 2;
@@ -18,10 +18,10 @@ function Output() {
                     i = savedText.length;
                     // if there is no closing tag, treats everyting after opening as a comment and skips right to the end
                 }
-            } else {
+            } else if (savedText.charAt(i) != '\n' || savedText.charAt(i-1) != '\n') { // prevent double line breaks after removing comments
                 newText += savedText.charAt(i);
-                i++;
             }
+            i++;
         }
         setText(newText);
     }, [])
@@ -29,7 +29,9 @@ function Output() {
     return (
         <>
             <h1>Output</h1>
-            <p>{text}</p>
+            <div style={{whiteSpace: "pre-wrap", margin: "auto", textAlign: 'left', width: "90%"}}>
+                {text}
+            </div>
         </>
     )
 }
