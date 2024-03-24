8a3cfdc87c3d629fcf9284d8d581cc9863a91938
slayerstokely <stokely@slayers.ru>
2024-03-24 15:27:49 +0200

    fixed output

--- src/components/Output.js
+++ src/components/Output.js
@@ -10,21 +10,21 @@ function Output() {
         while (i < savedText.length) {
             if (savedText.charAt(i) == '/' && savedText.charAt(i+1) == '/') {
                 i += savedText.slice(i).indexOf('\n');
+                while (savedText.charAt(i+1) == '\n') { // removes extra line breaks after removing comments
+                    i++;
+                }
                 // skips indexes between opening and closing tags
             } else if (savedText.charAt(i) == '/' && savedText.charAt(i+1) == '*') {
                 if (savedText.slice(i).includes('*/')) {
                     i += savedText.slice(i).indexOf('*/') + 1;
-                    if (savedText.charAt(i+1) == ' ') { // removes spaces at the beginning of the line after removing comments
+                    while (savedText.charAt(i+1) == ' ' || savedText.charAt(i+1) == '\n') { // removes spaces at the beginning of the line and extra line breaks after removing comments
                         i++;
-                    }
+                    } 
                 } else {
                     i = savedText.length;
                     // if there is no closing tag, treats everyting after opening as a comment and skips right to the end
                 }
             } else {
-                while (savedText.charAt(i+1) == '\n') { // removes extra line breaks after removing comments
-                    i++;
-                }
                 newText += savedText.charAt(i);
             }
             i++;
